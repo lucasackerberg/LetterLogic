2a40b50e65713010c98ce308cb875259a2d5a765
lucasackerberg <lucas@ackerberg.se>
2024-04-22 10:12:29 +0200

    footer
    footer

--- letterlogic/src/App.jsx
+++ letterlogic/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Header.jsx'
+import Footer from './Footer.jsx'
 
 function App() {
   const numRows = 5;
@@ -40,6 +41,7 @@ function App() {
         <div className="game-board">
         {renderBoard()}
         </div>
+        <Footer></Footer>
       </div>
     </>
   )
---
+++ letterlogic/src/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+function Footer() {
+    return(
+        <div className="footer-wrapper">
+            <h2>this is a footer</h2>
+        </div>
+    )
+}
+
+export default Footer
